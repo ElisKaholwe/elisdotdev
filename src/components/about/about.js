d651192f87c80bf89a52e2ab3475135310d8a0d9
eliskaholwe@gmail.com <eliskaholwe@gmail.com>
2024-09-07 23:24:36 +0300

    Phase II: Refactored

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,18 +13,19 @@ const AboutPage = () => {
           secondary school and knowing what I wanted, I dived headfirst into learning the
           <Link href={web} className={`${styles.special}`}>
             web
-          </Link>
-          I began with
+          </Link>{' '}
+          I began with{' '}
           <Link href={js} className={`${styles.special}`}>
             JavaScript
-          </Link>
+          </Link>{' '}
           and built the skeleton of my first web app, adding dynamic features to it for the first time, which was an amazing
           experience.
         </p>
         <p className={`${styles.paragraph}`}>
           In 2021, I enrolled in the Computer Science and Engineering program at the
           <Link href={udsm} className={`${styles.special}`}>
-            University of Dar es salaam (COICT)
+            {' '}
+            University of Dar es salaam (COICT){' '}
           </Link>
           Surrounded by experienced peers and lecturers, my understanding of web development deepened. I quickly mastered
           JavaScript frameworks and created an online suggestion box system for the university as my final year project. I always
@@ -34,7 +35,7 @@ const AboutPage = () => {
           Fast-forward to today, and I&apos;ve had the privilege of building innovative solutions as a front-end engineer, my main
           focus is crafting exceptional user experiences where design and engineering converge. I&apos;m also planning to release
           an online video course that covers everything one needs to know to build responsive web applications for
-          <span className={`${styles.special}`}>mobile</span> and <span className={`${styles.special}`}>desktop</span> with
+          <span className={`${styles.special}`}> mobile</span> and <span className={`${styles.special}`}> desktop</span> with
           integrated APIs
         </p>
       </div>
